601b1ae5c366eaed4a52e526bc18ac62e2d84b6f
AshakaE <ashakaegerega@gmail.com>
2023-05-10 11:34:32 +0100

    Add setup

--- src/utils/sendUpdates.ts
+++ src/utils/sendUpdates.ts
@@ -126,5 +126,9 @@ export default class Updates {
         }
       })
     }
+
+    if(messageUpdates.updateType === UpdateTypes.DOWNTIME) {
+        // TODO: send updates of the channels having downtime
+    }
   }
 }
