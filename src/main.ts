6590eb229eaaaaae55f74d04b09e3cf5945e463c
Ashaka Egerega <ashakaegerega@gmail.com>
2025-12-13 21:18:48 +0000

    Fix deprecated issues

--- src/main.ts
+++ src/main.ts
@@ -6,37 +6,37 @@ import 'dotenv/config'
 import { container } from 'tsyringe'
 
 export const client = new Client({
-    simpleCommand: {
-        prefix: "!",
-    },
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildMessageReactions,
-        IntentsBitField.Flags.GuildVoiceStates,
-    ],
-    // If you only want to use global commands only, comment this line
-    botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+  simpleCommand: {
+    prefix: "!",
+  },
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.GuildVoiceStates,
+  ],
+  // If you only want to use global commands only, comment this line
+  botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
 });
 
 
 client.once("clientReady", async () => {
-    // make sure all guilds are in cache
-    await client.guilds.fetch();
+  // make sure all guilds are in cache
+  await client.guilds.fetch();
 
-    // init all application commands
-    await client.initApplicationCommands();
+  // init all application commands
+  await client.initApplicationCommands();
 
   // init permissions; enabled log to see changes
-//   await client.initApplicationPermissions(true);
+  //   await client.initApplicationPermissions(true);
 
   // uncomment this line to clear all guild commands,
   // useful when moving to global commands from guild commands
   //  await client.clearApplicationCommands(
   //    ...client.guilds.cache.map((g) => g.id)
   //  );
-  
+
   console.log("Bot started");
 });
 
@@ -49,13 +49,13 @@ client.on("messageCreate", (message: Message) => {
 });
 
 async function run() {
-    
+
   DIService.engine = tsyringeDependencyRegistryEngine.setInjector(container)
-    // with cjs
-    // await importx(dirname + "/{events,commands}/**/*.{ts,js}");
-    // with ems
-    await importx(
-        dirname(import.meta.url) + "/{events,commands,services,utils}/**/*.{ts,js}"
+  // with cjs
+  // await importx(dirname + "/{events,commands}/**/*.{ts,js}");
+  // with ems
+  await importx(
+    dirname(import.meta.url) + "/{events,commands,services,utils}/**/*.{ts,js}"
   );
 
   // let's start the bot
@@ -63,7 +63,7 @@ async function run() {
     throw Error("Could not find BOT_TOKEN in your environment");
   }
   await client.login(process.env.BOT_TOKEN); // provide your bot token
-  
+
 }
 
 run();
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,12 +6,12 @@ export const writeFile = (arr: string[] | undefined, filePath: string) => {
     console.log(`⚠️ No data to write to ${filePath}`)
     return
   }
-  
+
   const dir = path.dirname(filePath)
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true })
   }
-  
+
   let cleanArray = arr.map((e: string) => `"${e}"`)
   try {
     fs.writeFile(filePath, `export const links = [${cleanArray}]\n`, err => {
